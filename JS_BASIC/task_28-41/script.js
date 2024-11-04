089e6eb539800ec2e6e22d217cafc26e12ee49ee
Vladlenalutsyuk <lutsyuk.v.i.23@gmail.com>
2024-11-05 00:37:40 +0300

    task 28-41

---
+++ JS_BASIC/task_28-41/script.js
@@ -0,0 +1,86 @@
+// Задание 28 №1
+let str28 = "Lutsyuk";
+alert(str28);
+
+let str28_1 = "Vladlena";
+alert(str28_1);
+
+// Задание 29 №1
+let str29 = "!!!";
+alert(str29);
+
+// Задание 29 №2
+let str29_1 = "java";
+let str29_2 = "script";
+alert(str29_1 + str29_2);
+
+// Задание 29 №3
+let str29_3 = "hello";
+let str29_4 = "world";
+alert(str29_3 + ' ' + str29_4);
+
+// Задание 30 №1
+let str30 = "vladlena";
+alert(str30.length);
+
+// Задание 31 №1
+let str31 = "xxx";
+let str31_1 = "yyy"
+let txt31 = `aaa ${str31} bbb ${str31_1} ccc`;
+alert(txt31);
+
+// Задание 32 №1
+let str32 = `a
+b
+c`;
+alert(str32);
+
+// Задание 33 №1
+let a_33;
+alert(a_33);
+
+// Задание 34 №1
+let a_34 = null;
+alert(a_34);
+
+// Задание 35 №1
+let a_35 = true;
+alert(a_35);
+
+// Задание 36 №1
+let a_36 = false;
+alert(a_36);
+
+// Задание 37 №1
+let str37 = "vlad";
+let str37_1 = "lena";
+alert(str37 * str37_1);
+
+// Задание 38 №1
+alert( 10 / 0); 
+
+// Задание 38 №2
+alert(-10 / 0);
+
+// Задание 39 №1
+let num39_1 = 5;
+console.log(num39_1);
+
+// Задание 39 №2
+let num39_2 = 5;
+let num39_3 = 15;
+let num39_4 = 25;
+console.log(num39_2, num39_3, num39_4);
+
+// Задание 40 №1
+console.log(10, -5, 0);
+console.log(3.14, -2.5, 1.0);
+console.log('hey');
+console.log(true, false);
+console.log('A', '!', '*');
+
+// Задание 41 №1
+alert(mistake);
+
+// Результат задания 41 №1
+// script.js:83 Uncaught ReferenceError: mistake is not defined at script.js:83:7
