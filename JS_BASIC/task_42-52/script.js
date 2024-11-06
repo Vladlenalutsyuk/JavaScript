3cbee93844f6573b27b92efecac78efd854fa742
Vladlenalutsyuk <lutsyuk.v.i.23@gmail.com>
2024-11-06 19:33:44 +0300

    task 42-52

---
+++ JS_BASIC/task_42-52/script.js
@@ -0,0 +1,278 @@
+// Задание 42 №1
+const PI = 3.14;
+r=6;
+alert(2*PI*r);
+
+// Задание 43 №1
+// let a = '5' + '2';
+// alert(a);
+// Результат выведния будет = 52
+
+// Задание 43 №2
+// let a = '5' + 2;
+// alert(a);
+// Результат выведния будет = 52
+
+// Задание 43 №3
+// let a = 5 + '2';
+// alert(a);
+// Результат выведния будет = 52
+
+// Задание 43 №4
+// let a = 5 + 2;
+// alert(a);
+// Результат выведния будет = 7
+
+// Задание 43 №5
+// let a = '5' * '2';
+// alert(a);
+// Результат выведния будет = 10
+
+// Задание 43 №6
+// let a = '5' - '2';
+// alert(a);
+// Результат выведния будет = 3
+
+// Задание 43 №7
+// let a = '5' / '2';
+// alert(a);
+// Результат выведния будет = 2,5
+
+// Задание 43 №8
+// let a = '5' % '2';
+// alert(a);
+// Результат выведния будет = 1
+
+// Задание 43 №9
+// let a = '5s' * '2';
+// alert(a);
+// Результат выведния будет = NaN
+
+// Задание 43 №10
+// let a = '5s' + '2';
+// alert(a);
+// Результат выведния будет = 5s2
+
+// Задание 43 №11
+// let a = (-'5') + (-'2');
+// alert(a);
+// Результат выведния будет = -7
+
+// Задание 43 №12
+// let a = '5' * 1 + '2' * 1;
+// alert(a);
+// Результат выведния будет = 7
+
+// Задание 43 №13
+// let a = '5' * '1' + '2' * '1';
+// alert(a);
+// Результат выведния будет = 7
+
+// Задание 43 №14
+// let a = '' + 3 + 1;
+// alert(a);
+// Результат выведния будет = 31
+
+// Задание 44 №1
+let a44_1= Number('10'); 
+let b44_1 = Number('20'); 
+alert(a44_1 + b44_1); 
+
+// Задание 44 №2
+// alert( Number('2') + Number('3') );
+// Результат выведния будет = 5
+
+// Задание 44 №3
+// alert( 2 + Number('3') );
+// Результат выведния будет = 5
+
+// Задание 44 №4
+// alert( '2' + Number('3') );
+// Результат выведния будет = 23
+
+// Задание 45 №1
+let a = '2';
+let b = '3';
+alert(+a + +b);
+
+// Задание 47 №1
+let a47_1 = parseInt('5px');
+let b47_1 = parseInt('6px');
+alert(a47_1 + b47_1);
+
+// Задание 47 №2
+let a47_2 = parseFloat('5.5px');
+let b47_2 = parseFloat('6.25px');
+alert(a47_2 + b47_2);
+
+// Задание 47 №3
+let a47_3 = parseFloat('5.5px');
+let b47_3 = parseFloat('6.25px');
+alert(a47_3 + b47_3 + 'px');
+
+// Задание 48 №1
+let a48 = 25;
+let b48 = 35;
+alert(String(a48) + String(b48)); 
+
+// Задание 49 №1
+let a49_1 = 123445;
+alert(String(a49_1).length);
+
+// Задание 49 №2
+let a49_2 = 5667; 
+let b49_2 = 76543;   
+let sum49_2 = String(a49_2).length + String(b49_2).length;
+alert(sum49_2);
+
+// Задание 50 №1
+// alert(true + 3);
+// Результат  4
+
+// Задание 50 №2
+// alert(true + true);
+// Результат  2
+
+// Задание 50 №3
+// alert(true - true);
+// Результат  0
+
+// Задание 50 №4
+// alert(true + false);
+// Результат 1
+
+// Задание 50 №5
+// alert('1' + true);
+// Результат  1true
+
+// Задание 50 №6
+// alert( String(true) + 1 );
+// Результат true1
+
+// Задание 50 №7
+// alert( String(true) + Number(true) );
+// Результат true1
+
+// Задание 51 №1
+// let test = Boolean(3);
+// alert(test)
+// Результат true
+
+// Задание 51 №2
+// let test = Boolean(0);
+// alert(test)
+// Результат false
+
+// Задание 51 №3
+// let test = Boolean(-1);
+// alert(test)
+// Результат  true
+
+// Задание 51 №4
+// let test = Boolean(-0);
+// alert(test)
+// Результат  false
+
+// Задание 51 №5
+// let test = Boolean(+0);
+// alert(test)
+// Результат false
+
+// Задание 51 №6
+// let test = Boolean('abc');
+// alert(test)
+// Результат  true
+
+// Задание 51 №7
+// let test = Boolean('');
+// alert(test)
+// Результат false
+
+// Задание 51 №8
+// let test = Boolean('0');
+// alert(test)
+// Результат true
+
+// Задание 51 №9
+// let test = Boolean(true);
+// alert(test)
+// Результат true
+
+// Задание 51 №10
+// let test = Boolean(false);
+// alert(test)
+// Результат false
+
+// Задание 51 №11
+// let test = Boolean('true');
+// alert(test)
+// Результат true
+
+// Задание 51 №12
+// let test = Boolean('false');
+// alert(test)
+// Результат true
+
+// Задание 51 №13
+// let test = Boolean(null);
+// alert(test)
+// Результат false
+
+// Задание 51 №14
+// let test = Boolean('null');
+// alert(test)
+// Результат true
+
+// Задание 51 №15
+// let test = Boolean(undefined);
+// alert(test)
+// Результат false
+
+// Задание 51 №16
+// let test = Boolean('undefined');
+// alert(test)
+// Результат true
+
+// Задание 51 №17
+// let test = Boolean(NaN);
+// alert(test);
+// Результат false
+
+// Задание 51 №18
+// let test = Boolean('NaN');
+// alert(test);
+// Результат true
+
+// Задание 51 №19
+// let test = Boolean(3 * 'abc');
+// alert(test);
+// Результат  false
+
+// Задание 51 №20
+// let test = Boolean(Infinity);
+// alert(test); 
+// Результат true
+
+// Задание 51 №21
+//let test = Boolean(1 / 0);
+// alert(test);
+// Результат  true
+
+// Задание 52 №1
+let str52_1 = 'abcde';
+alert(str52_1[0]); 
+alert(str52_1[2]); 
+alert(str52_1[4]); 
+
+// Задание 52 №2
+let str52_2 = 'abcde';
+alert(str52_2[4]); 
+alert(str52_2[3]); 
+alert(str52_2[2]); 
+alert(str52_2[1]); 
+alert(str52_2[0]);
+
+// Задание 52 №3
+let str52_3 = 'abcde';
+let num = 2; 
+alert(str52_3[num]);
