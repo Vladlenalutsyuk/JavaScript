56aea8a4ff0a3822fb6131095dff52181c67b7f6
Vladlenalutsyuk <lutsyuk.v.i.23@gmail.com>
2024-11-07 08:41:07 +0300

    task 68-75

---
+++ ARRAYS/task_68-75/script.js
@@ -0,0 +1,75 @@
+// Задание 68 №1
+let arr68_1 = [1, 2, 3, 4, 5]; 
+alert(arr68_1);
+console.log(arr68_1);
+
+// Задание 68 №2
+let arr68_2 = ["tiana"]; 
+alert(arr68_2);
+console.log(arr68_2);
+
+// Задание 69 №1
+let arr69_1 = [1, 2, 3]; 
+console.log(arr69_1[0]); 
+console.log(arr69_1[1]); 
+console.log(arr69_1[2]);
+
+// Задание 69 №2
+let arr69_2 = [1, 2, 3]; 
+console.log(arr69_2[0]); 
+console.log(arr69_2[1]); 
+console.log(arr69_2[2]);
+
+// Задание 69 №3
+let arr69_3 = [1, 2, 3];
+console.log(arr69_3[0] + arr69_3[1] + arr69_3[2]);
+
+// Задание 69 №4
+let arr69_4 = ['a', 'b', 'c', 'd'];
+console.log(String(arr69_4[0])+ "+" + String(arr69_4[1])+ '+' + String(arr69_4[2])+ '+' + String(arr69_4[3]));
+
+// Задание 70 №1
+let arr70_1 = ['disney', 'princess', 'tiana'];
+console.log(arr70_1.length) 
+
+// Задание 70 №2
+let arr70_2 = ['disney', 'princess', 'tiana', 'cartoon'];
+console.log(arr70_2[arr70_2.length - 1]) 
+
+let arr71_1 = ['a', 'b', 'c'];
+arr71_1[0] = '1';
+arr71_1[1] = '2';
+arr71_1[2] = '3';
+console.log(arr71_1);
+
+// Задание 72 №1
+let arr72 = [1, 2, 3];
+arr72[0] += 3;
+arr72[1] += 3;
+arr72[2] += 3;
+console.log(arr72);
+
+// Задание 73 №1
+let arr73 = [1, 2, 3];
+arr73[0]++;
+arr73[1]++;
+arr73[2]++;
+console.log(arr73);
+
+// Задание 74 №1
+let arr74 = [];
+arr74[0] = 1;
+arr74[1] = 2;
+arr74[2] = 3;
+console.log(arr74); 
+
+// Задание 74 №2
+arr74[3] = 4;
+arr74[4] = 5;
+console.log(arr74);
+
+// Задание 75 №1
+let arr75 = [];
+arr75[3] = 'a';
+arr75[8] = 'b';
+console.log(arr75.length);
