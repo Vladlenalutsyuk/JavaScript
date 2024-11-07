4b6678d807cca714d748ec3032ef92b7e1b442c1
Vladlenalutsyuk <lutsyuk.v.i.23@gmail.com>
2024-11-07 08:54:58 +0300

    task 76-79

---
+++ ARRAYS/task_76-79/script.js
@@ -0,0 +1,45 @@
+// Задание 76 №1
+let arr76_1 = [];
+arr76_1.push(1);
+arr76_1.push(2);
+arr76_1.push(3);
+console.log(arr76_1)
+
+// Задание 76 №2
+let arr76_2 = [1, 2, 3];
+arr76_2.push(4);
+arr76_2.push(5);
+console.log(arr76_2)
+
+// Задание 77 №1
+let arr77_1 = ['a', 'b', 'c'];
+let key77_1 = 2;
+console.log(arr77_1[key77_1]);
+
+// Задание 77 №2
+let arr77_2 = [1, 2, 3, 4, 5];
+let key77_2 = 1;
+let key77_3 = 2;
+console.log(arr77_2[key77_2] + arr77_2[key77_3]);
+
+// Задание 78 №1
+let arr78 = ['a', 'b', 'c', 'd', 'e']
+delete arr78[0];
+delete arr78[1];
+console.log(arr78.length)
+
+// Задание 79 №1
+let arr79_1 = [1, 2, 3, 4, 5];
+console.log(arr79_1[arr79_1.length-1]);
+
+// Задание 79 №2
+let arr79_2 = [1, 2, 3, 4, 5];
+console.log((arr79_2[0]) + (arr79_2[1]) + (arr79_2[2]) +(arr79_2[3]) + (arr79_2[4]));
+
+// Задание 79 №3
+let arr79_3 = [1, 2, 3, 4, 5];
+console.log(arr79_3.length);
+
+// Задание 79 №4
+let arr79_4 = [1, 2, 3, 4, 5];
+console.log(arr79_4.length);
