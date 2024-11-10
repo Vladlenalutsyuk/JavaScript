66e7bb188a14e3b6f972385cdd24dd4a059bd450
Vladlenalutsyuk <lutsyuk.v.i.23@gmail.com>
2024-11-10 20:22:11 +0300

    105-120

---
+++ CONDITIONS(104-142)/task_105-115/script.js
@@ -0,0 +1,429 @@
+// Задание 105 №1
+let test105_1 = 15;
+if (test105_1 > 10) {
+	console.log('+++'); 
+} else {
+	console.log('---');
+}
+
+// Задание 105 №2
+let test105_2 = 9;
+if (test105_2 < 10) {
+	console.log('+++'); 
+} else {
+	console.log('---');
+}
+
+// Задание 105 №3
+let test105_3 = 10;
+if (test105_3 >= 10) {
+	console.log('+++'); 
+} else {
+	console.log('---');
+}
+
+// Задание 105 №4
+let test105_4 = 2;
+if (test105_4 <= 10) {
+	console.log('+++'); 
+} else {
+	console.log('---');
+}
+
+// Задание 106 №1
+let test106 = 10;
+if (test106 == 10) {
+	console.log('+++'); 
+} else {
+	console.log('---');
+}
+
+// Задание 107 №1
+let test107 = 15;
+if (test107 != 10) {
+	console.log('+++'); 
+} else {
+	console.log('---');
+}
+
+// Задание 108 №1
+let test108_1 = 10;
+let test108_2 = 67;
+if (test108_1 > test108_2) {
+	console.log('test108_1 > test108_2'); 
+} else {
+	console.log('test108_1 < test108_2');
+}
+
+// Задание 108 №2
+let test108_3 = 10;
+let test108_4 = 10;
+if (test108_3 == test108_4) {
+	console.log('test108_3 = test108_4'); 
+} else {
+	console.log('test108_3 != test108_4');
+}
+
+// Задание 109 №1
+let test109_1 = 'asd';
+let test109_2 = 'asdfg';
+if (test109_1 == test109_2) {
+	console.log('test109_1 = test109_2'); 
+} else {
+	console.log('test109_1 != test109_2');
+}
+
+// Задание 110 №1
+let test110_1 = '123';
+let test110_2 = 123;
+if (test110_1 == test110_2) {
+	console.log('test110_1 = test110_2'); 
+} else {
+	console.log('test110_1 != test110_2');
+}
+
+// Задание 111 №1
+// let test1 = '3';
+// let test2 = '3';		
+// if (test1 == test2) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+// Выведится результат = +++
+
+// Задание 111 №2
+// let test1 = '3';
+// let test2 = '3';	
+// if (test1 === test2) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+// Выведится результат = +++
+
+// Задание 111 №3
+// let test1 = 3;
+// let test2 = '3';		
+// if (test1 == test2) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+// Выведится результат = +++
+
+// Задание 111 №4
+// let test1 = 3;
+// let test2 = '3';	
+// if (test1 === test2) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+// Выведится результат = ---
+
+// Задание 111 №5
+// let test1 = 3;
+// let test2 = 3;		
+// if (test1 === test2) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+// Выведится результат = +++
+
+// Задание 112 №1
+// let test1 = '3';
+// let test2 = '3';
+// if (test1 != test2) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+// Выведится результат = ---
+
+// Задание 112 №2
+// let test1 = '3';
+// let test2 = '3';		
+// if (test1 !== test2) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+// Выведится результат = ---
+
+// Задание 112 №3
+// let test1 = 3;
+// let test2 = '3';	
+// if (test1 != test2) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+// Выведится результат = ---
+
+// Задание 112 №4
+// let test1 = 3;
+// let test2 = '3';		
+// if (test1 !== test2) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+// Выведится результат = +++
+
+// Задание 112 №5
+// let test1 = 3;
+// let test2 = 2;	
+// if (test1 !== test2) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+// Выведится результат = +++
+
+// Задание 113 №1
+let num113_1 = 3;
+if (num113_1 > 0 && num113_1 < 5) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+// Задание 113 №2
+let num113_2 = 10;
+if (num113_2 >= 10 && num113_2 <= 20) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+// Задание 113 №3
+let num113_3 = 1;
+let num113_4 = 6;
+if (num113_3 <= 1 && num113_4 >= 3) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+// Задание 114 №1
+// let num1 = -10;
+// let num2 = -10;
+// if (num1 >= 0 && num2 >= 0) {
+//   console.log('+++');
+// } else {
+//   console.log('---');
+// }
+// Выведется результат = ---
+ 
+// Задание 114 №2
+// let num1 = 0;
+// let num2 = 0;
+// if (num1 >= 0 && num2 >= 0) {
+//   console.log('+++');
+// } else {
+//   console.log('---');
+// }
+// Выведется результат = +++
+        
+// Задание 114 №3
+// let num1 = 0;
+// let num2 = 5;
+// if (num1 >= 0 && num2 >= 0) {
+//   console.log('+++');
+// } else {
+//   console.log('---');
+// }
+// Выведется результат = +++
+
+// Задание 114 №4
+// let num1 = 5;
+// let num2 = 5;
+// if (num1 >= 0 && num2 >= 0) {
+//   console.log('+++');
+// } else {
+//   console.log('---');
+// }
+// Выведется результат = +++
+
+// Задание 114 №5
+// let num1 = -5;
+// let num2 = 15;
+// if (num1 >= 0 && num2 >= 0) {
+//   console.log('+++');
+// } else {
+//   console.log('---');
+// }
+// Выведется результат = ---
+
+// Задание 114 №6
+// let num1 = -5;
+// let num2 = 15;
+// if (num1 >= 0 && num2 >= 0) {
+//   console.log('+++');
+// } else {
+//   console.log('---');
+// }
+// Выведется результат = ---
+
+// Задание 114 №7
+// let num = 2;
+// if (num == 0 || num == 1) {
+//   console.log('+++');
+// } else {
+//   console.log('---');
+// }
+// Выведется результат = ---
+
+// Задание 114 №8
+// let num = 2;
+// if (num == 0 || num == 1 || num == 2) {
+//   console.log('+++');
+// } else {
+//   console.log('---');
+// }
+// Выведется результат = +++
+
+// Задание 115 №1
+// if (num == 0 || num > 1 && num < 5) {
+//     console.log('+++');
+//   } else {
+//     console.log('---');
+//   }
+// В JavaScript оператор && имеет более высокий приоритет, чем ||. 
+// Поэтому порядок выполнения будет следующим:
+// 1. Проверка num > 1 && num < 5.
+// 2. Результат этой проверки комбинируется с num == 0 с помощью ||.
+
+// Задание 116 №1
+let num116_1 = 3;
+if ((num116_1 > 5 && num116_1 < 10) || num116_1 == 20) {
+	console.log('+++');
+} else {
+	console.log('---');
+} 
+
+// Задание 116 №2
+let num116_2 = 3;		
+if (num116_2 > 5 || (num116_2 > 0 && num116_2 < 3)) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+// Задание 116 №3
+let num116_3 = 3;
+if (num116_3 == 9 || (num116_3 > 10 && num116_3 < 20) || (num116_3 > 20 && num116_3 < 30)) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+// Задание 117 №1
+let num117_1 = 5;
+let num117_2 = 1;
+if (! (num117_1 < 0 || num117_2 > 10) ) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+// Задание 118 №1
+let test118_1 = true;
+
+if (test118_1 === true) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+// Задание 118 №2
+let test118_2 = false;
+
+if (test118_2 === false) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+// Задание 119 №1
+// При приведении к логическому типу (boolean) значения, которые дают false, включают:
+// 1. false
+// 2. 0 
+// 3. -0 
+// 4. NaN 
+// 5. "" — пустая строка.
+// 6. null 
+// 7. undefined 
+
+// Задание 119 №2
+// let test = 1;	
+// if (test == true) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+// Результат вывода = +++
+
+// Задание 119 №3
+// let test = 0;  
+// if (test == true) {
+//   console.log('+++');
+// } else {
+//   console.log('---');
+// }
+// Результат вывода = --- 
+
+// Задание 119 №4
+// let test = 1;  
+// if (test == false) {
+//   console.log('+++');
+// } else {
+//   console.log('---');
+// }
+// Результат вывода = --- 
+
+// Задание 119 №5
+// let test = 1;  
+// if (test != true) {
+//   console.log('+++');
+// } else {
+//   console.log('---');
+// }
+// Результат вывода = --- 
+
+// Задание 119 №6
+// let test = '';  
+// if (test == false) {
+//   console.log('+++');
+// } else {
+//   console.log('---');
+// }
+// Результат вывода = +++ 
+
+// Задание 119 №7
+// let test;  
+// if (test == true) {
+//   console.log('+++');
+// } else {
+//   console.log('---');
+// }
+// Результат вывода = --- 
+
+// Задание 119 №8
+// let test = 3 * 'a';  
+// if (test == true) {
+//   console.log('+++');
+// } else {
+//   console.log('---');
+// }
+// Результат вывода = --- 
+
+// Задание 120 №1
+let test120 = true;
+if (test120) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
