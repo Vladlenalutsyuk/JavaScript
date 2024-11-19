96efb57c6b1d3c318eb0a7825d3f458116c179ee
Vladlenalutsyuk <lutsyuk.v.i.23@gmail.com>
2024-11-19 23:42:22 +0300

    225

--- TYPES_OF_FUNCTIONS(223-239)/script.js
+++ TYPES_OF_FUNCTIONS(223-239)/script.js
@@ -20,3 +20,16 @@ console.log(func224);
 // Задание 224 №4
 func224 = 123; 
 console.log(func224); 
+
+// Задание 225 №1
+console.log('Результат задания 225:');
+// Задание 1
+function func225_1() {
+	return 3;
+}
+
+// Задание 225 №2
+const func225_2 = func225_1;
+
+// Задание 225 №3
+console.log(func225_1() + func225_2()); 
