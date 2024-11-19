07388c65cf7b19576224ad99e79c4c4452079f44
Vladlenalutsyuk <lutsyuk.v.i.23@gmail.com>
2024-11-19 23:27:05 +0300

    216-222

---
+++ FUNCTION_VARIABLES(216-222)/script.js
@@ -0,0 +1,245 @@
+// Задание 216 №1
+// let num = 1;
+// function func() {
+//     console.log(num);
+// }
+// func(); //Результат выполнения задания = 1
+
+// Задание 216 №2
+// let num = 1;
+// function func() {
+//     console.log(num);
+// }
+// num = 2;
+// func(); // Результат выполнения задания = 2  
+
+// Задание 216 №3
+// function func() {
+//     console.log(num);
+// }
+// let num;
+// num = 1;
+// func(); // Результат выполнения задания = 1
+// num = 2;
+// func(); // Результат выполнения задания = 2
+
+// Задание 217 №1
+// function func() {
+// 	let num = 5;
+// 	return num;
+// }
+// console.log(num); // Результат выполнения задания = выдаст ошибку
+
+// Задание 217 №2
+// function func() {
+// 	let num = 5;
+// 	return num;
+// }
+// console.log(func()); // Результат выполнения задания = выдаст ошибку
+
+// Задание 218 №1
+// let num = 1;
+// function func() {
+// 	num = 2;
+// }
+// func();
+// console.log(num); // Результат выполнения задания = 2
+
+// Задание 218 №2
+// let num = 1;
+// function func() {
+// 	let num = 2;
+// }
+// func();
+// console.log(num); // Результат выполнения задания = 1
+
+// Задание 219 №1
+// let num = 1;
+// function func() {
+//   num = 2;
+// }
+// func();
+// console.log(num); // Результат выполнения задания = 2
+
+// Задание 219 №2
+// let num = 1;
+// function func() {
+//   num = 2;
+// }
+// console.log(num); // Результат выполнения задания = 1
+
+// Задание 219 №3
+// let num = 1;
+// function func() {
+//   let num = 2;
+// }
+// func();
+// console.log(num); // Результат выполнения задания = 1 
+
+// Задание 219 №4
+// let num = 1;
+// function func() {
+//   let num = 2;
+// }
+// console.log(num);
+// func(); // Результат выполнения задания = 1 
+
+// Задание 219 №5
+// let num = 1;
+// function func() {
+//   num = 2;
+// }
+// console.log(num);
+// func(); // Результат выполнения задания = 1 
+
+// Задание 219 №6
+// let num = 1;
+// function func() {
+//   num++;
+// }
+// func();
+// func();
+// func();
+// console.log(num); // Результат выполнения задания = 4
+
+// Задание 219 №7
+// function func() {
+//   num = 2;
+// }
+// let num = 1;
+// console.log(num);
+// func(); // Результат выполнения задания = 1
+
+// Задание 219 №8
+// function func() {
+//   num = 2;
+// }
+// let num = 1;
+// func();
+// console.log(num); // Результат выполнения задания = 2
+
+// Задание 220 №1
+// function func(localNum) {
+//   console.log(localNum);
+// }
+// func(1); // Результат выполнения задания = 1
+
+// Задание 220 №2
+// function func(localNum) {
+//   console.log(localNum);
+// }
+// let num = 1;
+// func(num); // Результат выполнения задания = 1
+
+// Задание 220 №3
+// function func(localNum) {
+//   console.log(localNum);
+// }
+// let num = 1;
+// func(num);
+// num = 2; // Результат выполнения задания = 1
+
+// Задание 220 №4
+// let num = 1;
+// function func(localNum) {
+//   console.log(localNum);
+// }
+// num = 2;
+// func(num); // Результат выполнения задания = 2
+
+// Задание 220 №5
+// function func(localNum) {
+//   localNum = 2;
+// }
+// let num = 1;
+// func(num);
+// console.log(num); // Результат выполнения задания = 1
+
+// Задание 220 №6
+// function func(localNum) {
+//   num = 2;
+// }
+// let num = 1;
+// func(num);
+// console.log(num); // Результат выполнения задания = 2
+
+// Задание 220 №7
+// function func(localNum) {
+//   let num = 2;
+// }
+// let num = 1;
+// func(num);
+// console.log(num); // Результат выполнения задания = 1
+
+// Задание 221 №1
+// function func(num) {
+//   num = 2;
+// }
+// let num = 1;
+// func(num);
+// console.log(num); // Результат выполнения задания = 1
+
+// Задание 221 №2
+// function func() {
+//   num = 2;
+// }
+// let num = 1;
+// func();
+// console.log(num); // Результат выполнения задания = 2
+
+// Задание 221 №3
+// function func() {
+//   let num = 2;
+// }
+// let num = 1;
+// func();
+// console.log(num); // Результат выполнения задания = 1
+
+// Задание 222 №1
+// function func(obj) {
+//   obj.a = '!';
+// }
+// let obj = {a: 1, b: 2, c: 3};
+// func(obj);
+// console.log(obj); // Результат выполнения задания = {a: '!', b: 2, c: 3}
+
+// Задание 222 №2
+// function func(arg) {
+//   arg = '!';
+// }
+// let obj = {a: 1, b: 2, c: 3};
+// func(obj.a);
+// console.log(obj); // Результат выполнения задания = {a: 1, b: 2, c: 3}
+
+// Задание 222 №3
+// function func(obj) {
+//   obj = '!';
+// }
+// let obj = {a: 1, b: 2, c: 3};
+// func(obj.a);
+// console.log(obj); // Результат выполнения задания = {a: 1, b: 2, c: 3}
+
+// Задание 222 №4
+// function func(arr) {
+//   arr.splice(1, 1);
+// }
+// let arr = [1, 2, 3];
+// func(arr);
+// console.log(arr); // Результат выполнения задания = [1, 3]
+
+// Задание 222 №5
+// function func(arr) {
+//   arr.slice(1, 1);
+// }
+// let arr = [1, 2, 3];
+// func(arr);
+// console.log(arr); // Результат выполнения задания = [1, 2, 3]
+
+// Задание 222 №6
+// function func(arr) {
+//   let newArr = arr;
+//   newArr[0] = '!';
+// }
+// let arr = [1, 2, 3];
+// func(arr);
+// console.log(arr); // Результат выполнения задания = ['!', 2, 3]
